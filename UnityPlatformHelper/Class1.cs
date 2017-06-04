9cc3e028a35e21bc659feeb9fb831f77f171af26
rms <rms@rms80.com>
2017-06-03 21:23:52 -0400

    initial checkin

---
+++ UnityPlatformHelper/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UnityPlatformHelper
+{
+    public class OpenFileTest
+    {
+        public void DoImportSTLDialog()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            DialogResult result = dlg.ShowDialog();
+        }
+    }
+}
